d6e9319fe4dfe27d3791760cc84dbc2e627c16d8
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-04-25 14:49:47 -0500

    feat: :lipstick: Se agregaron las categorias en pantalla y su diseño

---
+++ components/campus-category-filters.js
@@ -0,0 +1,104 @@
+class CategoriesFilter extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({mode: "open"});
+        const nav = document.createElement("nav");
+        const style = document.createElement("style");
+        style.textContent = `
+            nav {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: var(--color-bg);
+                padding: 1rem;
+            }
+            ul {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                list-style: none;
+            }
+            li {
+                margin: 0 1rem;
+            }
+            button {
+                width: 10rem;
+                height: 2rem;
+                background:rgb(248, 73, 73);
+                border: none;
+                border-radius: 0.5rem;
+                color: #fff5f5fd;
+                font-size: 1rem;
+                font-weight: bold;
+                cursor: pointer;
+            }
+
+            #todas{
+                background: #000;
+                
+            }
+            button:hover {
+                transform: scale(1.1);
+                background-color: #000;
+            }
+        `
+
+        nav.id = 'all-categories';
+        nav.innerHTML = `
+            <ul>
+                <li><button id="todas">Todas</button></li>
+                <li><button id="eventos">Eventos</button></li>
+                <li><button id="investigacion">Investigación</button></li>
+                <li><button id="empleabilidad">Empleabilidad</button></li>
+                <li><button id="vida-estudiantil">Vida Estudiantil</button></li>
+            </ul>
+        `
+        shadow.append(style,nav);
+    }
+    connectedCallback() {
+        this.shadowRoot.getElementById('todas').addEventListener('click', () => {
+            alert('Todas las noticias');
+            this.dispatchEvent(new CustomEvent('filter:category',{
+                detail: {action: 'all'},
+                bubbles: true,
+                composed: true
+            }));
+            
+        });
+        this.shadowRoot.getElementById('eventos').addEventListener('click', () => {
+            alert('Eventos');
+            this.dispatchEvent(new CustomEvent('filter:category',{
+                detail: {action: 'eventos'},
+                bubbles: true,
+                composed: true
+            }));
+        });
+        this.shadowRoot.getElementById('investigacion').addEventListener('click', () => {
+            alert('Investigación');
+            this.dispatchEvent(new CustomEvent('filter:category',{
+                detail: {action: 'investigacion'},
+                bubbles: true,
+                composed: true
+            }));
+        });
+        this.shadowRoot.getElementById('empleabilidad').addEventListener('click', () => {
+            alert('Empleabilidad');
+            this.dispatchEvent(new CustomEvent('filter:category',{
+                detail: {action: 'empleabilidad'},
+                bubbles: true,
+                composed: true
+            }));
+        });
+        this.shadowRoot.getElementById('vida-estudiantil').addEventListener('click', () => {
+            alert('Vida Estudiantil');
+            this.dispatchEvent(new CustomEvent('filter:category',{
+                detail: {action: 'vida-estudiantil'},
+                bubbles: true,
+                composed: true
+            }));
+        });
+    }
+}
+customElements.define("campus-category-filters", CategoriesFilter);
+
+export default CategoriesFilter;
---
+++ main.js
@@ -0,0 +1,76 @@
+import './components/campus-category-filters.js';
+
+const campusArticles = [
+    {
+        id: 1,
+        title: "Jornada de puertas abiertas en Ingeniería",
+        summary: "Visitas guiadas y charlas con profesores y estudiantes.",
+        content: "<p>Este sábado 3 de mayo...</p>",
+        author: "Oficina de Admisiones",
+        date: "28 de abril, 2025",
+        category: "Eventos"
+    },
+    {
+        id: 2,
+        title: "Proyecto de robótica gana concurso nacional",
+        summary: "El equipo RoboCanino de Informática obtuvo el primer lugar.",
+        content: "<p>Tras meses de trabajo...</p>",
+        author: "Facultad de Ingeniería",
+        date: "25 de abril, 2025",
+        category: "Investigación"
+    },
+    {
+        id: 3,
+        title: "Horas de trabajo dan fruto",
+        summary: "El equipo de empleabilidad, logra un incremento de 10% de empleos.",
+        content: "<p>Desde el 1 de abril...</p>",
+        author: "Oficina de Empleabilidad",
+        date: "22 de abril, 2025",
+        category: "Empleabilidad"
+    },
+    {
+        id: 4,
+        title: "Hackaton abierto para la comunidad",
+        summary: "Se ha realizado una sesión de actividades con premios y certificados.",
+        content: "<p>Este miercoles 23 de abril...</p>",
+        author: "Oficina de Admisiones",
+        date: "24 de abril, 2025",
+        category: "Eventos"
+    },
+    {
+        id: 5,
+        title: "¿Qué opinan los estudiantes de su trabajo?",
+        summary: "Se ha preguntado a varios estudiantes cómo les gustaría trabajar y qué hacen por ello.",
+        content: "<p>Esta tarde 27 de abril...</p>",
+        author: "Oficina de Investigación",
+        date: "27 de abril, 2025",
+        category: "Vida Estudiantil"
+    },
+    {
+        id: 6,
+        title: "Torneo de ping pong",
+        summary: "Tras una semana de prácticas, Se realizó el torneo de ping pong.",
+        content: "<p>Desde el 20 de abril...</p>",
+        author: "Camper Felipe",
+        date: "28 de mayo, 2025",
+        category: "Eventos"
+    },
+    {
+        id: 7,
+        title: "¿Será el fin de la programación?",
+        summary: "Tras un debate entre estudiantes sobre la IA, se pregunta el destino de la programación.",
+        content: "<p>Independientemente de la fecha...</p>",
+        author: "Oficina de Investigación",
+        date: "1 de mayo, 2025",
+        category: "Investigación"
+    },
+    {
+        id: 8,
+        title: "Nueva metodología de aprendizaje",
+        summary: "Se ha presentado una nueva metodología de aprendizaje por los mismos estudiantes.",
+        content: "<p>Desde el año pasado...</p>",
+        author: "Camper Felipe",
+        date: "29 de abril, 2025",
+        category: "Investigación"
+    },
+  ];
